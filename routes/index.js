8ba573a4c577b6d04456e5fa0800ab0acde670b0
fakhrilak <fakhrilak23@gmail.com>
2022-02-28 19:36:27 +0700

    y

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {login,register, getSinge, loadUser} = require("../controllers/auth");
-const {auth} = require("../midleware/auth")
-const {upload} = require("../midleware/uploadImage")
-const {uploadsfile, getFoto, deletFoto,}= require("../controllers/uploads");
-const { createPost, getPost,getPostID } = require('../controllers/post');
+const { login, register, getSinge, loadUser } = require("../controllers/auth");
+const { auth } = require("../midleware/auth");
+const { upload } = require("../midleware/uploadImage");
+const { uploadsfile, getFoto, deletFoto } = require("../controllers/uploads");
+const { createPost, getPost, getPostID } = require("../controllers/post");
 
+router.post("/register", register);
+router.post("/login", login);
+router.get("/auth", auth, loadUser);
 
-router.post("/register",register)
-router.post("/login",login)
-router.get("/auth",auth,loadUser)
+router.post("/uploads", auth, upload("file"), uploadsfile);
+router.get("/uploads", getFoto);
+router.patch("/uploads", auth, deletFoto);
 
-router.post("/uploads",auth,upload("file"),uploadsfile)
-router.get("/uploads",getFoto)
-router.patch("/uploads",auth,deletFoto)
+router.get("/single/:name", getSinge);
+router.post("/post", auth, upload("file"), createPost);
+router.get("/post", getPost);
+router.get("/post/:id", getPostID);
 
-router.get("/single/:name",getSinge)
-router.post("/post",auth,upload('file'),createPost)
-router.get("/post",auth,getPost)
-router.get("/post/:id",auth,getPostID)
-
-module.exports = router
+module.exports = router;
